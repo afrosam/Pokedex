5e6b27e2cb6b591588ad1bab4b0b1c7689c668aa
Sam Trettin <trettinftw@gmail.com>
2019-01-08 16:25:21 -0800

    Updates

---
+++ precache-manifest.2e7c00fa3ebf964d29df9956eb171b30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1681525083bdf9d03105c268e3eb9d2c",
+    "url": "/Pokedex/static/media/Level_Up_Pokemon_Red.16815250.mp3"
+  },
+  {
+    "revision": "b3468cd3de89372a17d9",
+    "url": "/Pokedex/static/js/runtime~main.b3468cd3.js"
+  },
+  {
+    "revision": "ded7e019885c39e8d69c",
+    "url": "/Pokedex/static/js/main.ded7e019.chunk.js"
+  },
+  {
+    "revision": "25121609755356f7afa6",
+    "url": "/Pokedex/static/js/1.25121609.chunk.js"
+  },
+  {
+    "revision": "ded7e019885c39e8d69c",
+    "url": "/Pokedex/static/css/main.3754ea35.chunk.css"
+  },
+  {
+    "revision": "2fd2e44f2b0ec96482068561e53c15cc",
+    "url": "/Pokedex/index.html"
+  }
+];
